47a14238ef0ea636f96a8d4e58d6faac766812a6
ILdar Shamiev <shamief@yandex.ru>
2018-03-05 15:36:17 +0300

    question_form_helper, render tags in base.html

--- website/views.py
+++ website/views.py
@@ -1,3 +1,5 @@
+from collections import namedtuple
+
 from django.shortcuts import render
 from django.shortcuts import redirect
 from django.shortcuts import render_to_response
@@ -14,6 +16,7 @@ from django.contrib.auth import authenticate, login, logout
 from django.utils.translation import ugettext_lazy as _
 from django.contrib.auth.models import User
 
+from .models import Tag
 from .models import Question
 from .models import Answer
 from .models import Profile
@@ -23,8 +26,9 @@ from .forms import UserCreateForm
 from .forms import ProfileCreateForm
 
 
-def create_or_get_question_form(request):
+def question_form_helper(request):
     """ Вспомогательная функция создания формы вопроса"""
+    tags = []
     if request.method == 'POST':
         question_form = QuestionCreateForm(request.POST)
         if question_form.is_valid():
@@ -33,18 +37,22 @@ def create_or_get_question_form(request):
             question.save()
             request.session['question_id'] = str(question.pk)
     else:
+        tags = Tag.objects.all()
         question_form = QuestionCreateForm()
-    return question_form
+    QuestionHelper = namedtuple('QuestionHelper', ('question_form', 'tags'))
+    return QuestionHelper(question_form, tags)
 
 
 def question_list_view(request):
     """Главная страница со списком вопросов"""
-    question_form = create_or_get_question_form(request)
+    question_helper = question_form_helper(request)
     if request.method == 'POST':
-        question = question_form.save(commit=False)
+        question = question_helper.question_form.save(commit=False)
         return redirect('question', str(question))
     questions = Question.objects.all()
-    return render(request, 'list.html', {'form': question_form, 'questions': questions})
+    return render(request, 'list.html', {'questions': questions,
+                                         'form': question_helper.question_form,
+                                         'tags': question_helper.tags})
 
 
 def search(request):
@@ -57,7 +65,7 @@ def index(request):
 
 def question(request, header):
     """ Страница вопроса со списком ответов """
-    question_form = create_or_get_question_form(request)
+    question_helper = question_form_helper(request)
     question_id = request.session.pop('question_id', None)
     if question_id:
         question_query = Question.objects.filter(pk=question_id)
@@ -65,11 +73,13 @@ def question(request, header):
         header = header.replace('-', ' ')
         question_query = Question.objects.filter(header=header)
     if request.method == 'POST':
-        question = question_form.save(commit=False)
+        question = question_helper.question_form.save(commit=False)
         return redirect('question', str(question))
 
     return render(request, 'question.html',
-                  context={'questions': question_query, 'form': question_form})
+                  context={'questions': question_query,
+                           'form': question_helper.question_form,
+                           'tags': question_helper.tags})
 
 
 def tag(request):
@@ -84,12 +94,12 @@ def logout_view(request):
 def signup_view(request):
     """ Страница регистрации """
     question_form = ()
+    tags = []
     if request.user.is_authenticated:
         # форма вопроса на странице регистрации
-        if request.method == 'GET':
-            question_form = QuestionCreateForm()
-        else:
-            question_form = create_or_get_question_form(request)
+        question_helper = question_form_helper(request)
+        question_form = question_helper.question_form
+        tags = question_helper.tags
         if question_form.is_bound:
             question = question_form.save(commit=False)
             return redirect('question', str(question))
@@ -109,20 +119,18 @@ def signup_view(request):
     else:
         user_form = UserCreateForm()
         profile_form = ProfileCreateForm()
-    return render(request, 'signup.html', {
-        'user_form': (user_form, profile_form),
-        'form': question_form,
-    })
+    return render(request, 'signup.html', {'user_form': (user_form, profile_form),
+                                           'form': question_form, 'tags': tags})
 
 
 def settings(request):
     """ Страница настроек пользователя """
     if request.user.is_authenticated:
+        question_helper = question_form_helper(request)
         if request.method == 'POST':
             # форма вопроса на странице настроек
-            question_form = create_or_get_question_form(request)
-            if question_form.is_bound:
-                question = question_form.save(commit=False)
+            if question_helper.question_form.is_bound:
+                question = question_helper.question_form.save(commit=False)
                 return redirect('question', str(question))
 
             user_form = UserForm(request.POST, instance=request.user)
@@ -132,16 +140,13 @@ def settings(request):
                 profile_form.save()
                 return redirect('ask')
         else:
-            question_form = QuestionCreateForm()
-
             user = User.objects.get(pk=request.user.pk)
             profile = Profile.objects.get(user_id=request.user.pk)
             user_form = UserForm(instance=user)
             profile_form = ProfileCreateForm(instance=profile)
-        return render(request, 'settings.html', {
-            'user_form': (user_form, profile_form),
-            'form': question_form,
-        })
+        return render(request, 'settings.html', {'user_form': (user_form, profile_form),
+                                                 'form': question_helper.question_form,
+                                                 'tags': question_helper.tags, })
     else:
         return redirect('ask')
 
