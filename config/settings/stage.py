b92bf923e0cb10197a8ccd2d8200a00e9a271117
ILdar Shamiev <shamief@yandex.ru>
2018-03-26 21:38:56 +0300

    whitenoise serve static on stage

--- config/settings/stage.py
+++ config/settings/stage.py
@@ -56,4 +56,15 @@ EMAIL_HOST = 'localhost'
 # https://docs.djangoproject.com/en/dev/ref/settings/#email-port
 EMAIL_PORT = 1025
 
-STATIC_ROOT = root('static')
+MIDDLEWARE = [
+    #'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
